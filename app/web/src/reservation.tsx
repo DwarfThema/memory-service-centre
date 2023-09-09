a8b1487c8d0c3394d39dbada013ce9bf72524859
unknown <dwarfthema@gmail.com>
2023-09-09 18:20:47 +0900

    ourService_Artificial Hippocampus

---
+++ app/web/src/reservation.tsx
@@ -0,0 +1,17 @@
+export default function Reservation() {
+  return (
+    <div className="h-[55%] w-[55%] bg-contain bg-no-repeat bg-center bg-[url('/textures/web-home/Base.png')] flex text-center justify-center items-center flex-col">
+      <span className="text-xl mt-10 mr-6 w-[65%]">
+        We offer online reservations for your convenience. <br />
+        <br />
+        If you apply for an online reservation, we will send you a text message
+        after confirmation. <br />
+        <br />
+        Please enter the code MM at least one week before your scheduled
+        surgery. Once you receive the confirmation text, your reservation is
+        complete.
+        <br />
+      </span>
+    </div>
+  );
+}
