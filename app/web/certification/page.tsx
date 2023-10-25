04e676c2505d3b7e9bfee27e93e876237a2ff9a5
Junho Park <dwarfthema@gmail.com>
2023-10-25 09:25:16 +0900

    certification_fix data placeholder

--- app/web/certification/page.tsx
+++ app/web/certification/page.tsx
@@ -162,8 +162,9 @@ export default function Home() {
                     type="date"
                     name="BirthData"
                     className="border-2 border-gray-400 border-solid px-2 w-[270px]"
+                    placeholder="YYYY-MM-DD"
                     required
-                    pattern="\d{4}-\d{2}-\d{2}"
+                    pattern="(?:19|20)\[0-9\]{2}-(?:(?:0\[1-9\]|1\[0-2\])-(?:0\[1-9\]|1\[0-9\]|2\[0-9\])|(?:(?!02)(?:0\[1-9\]|1\[0-2\])-(?:30))|(?:(?:0\[13578\]|1\[02\])-31))"
                     onChange={onDobChange}
                   />
                 </div>
